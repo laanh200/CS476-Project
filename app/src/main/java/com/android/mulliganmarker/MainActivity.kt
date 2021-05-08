4667f90632b6e791818d00f0db2d5081060b8663
laanh200 <79466152+laanh200@users.noreply.github.com>
2021-05-07 18:12:14 -0600

    Inserted sliding nav menu. Databinding in the gradle.

--- app/src/main/java/com/android/mulliganmarker/MainActivity.kt
+++ app/src/main/java/com/android/mulliganmarker/MainActivity.kt
@@ -1,15 +1,38 @@
 package com.android.mulliganmarker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    //Navigation view
+    private lateinit var navView: NavigationView
+
+    //Drawer Layout
+    private lateinit var drawer:DrawerLayout
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
 
         //hide the Back arrow of the main activity to the welcome screen
         supportActionBar?.setDisplayHomeAsUpEnabled(false)
 
     }
+
+
+
 }
